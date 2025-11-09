9f9e8fd953e8986751cf9b1ebaab75a928e1c9c5
pinoybai <jagadeepana@gmail.com>
2025-11-09 09:42:10 +0530

    Fix base path for GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://timestones.today",
-  base: "/",
+  site: "https://pinoybai.github.io",
+  base: "/timestoneslanding",
   output: "static",
 });
